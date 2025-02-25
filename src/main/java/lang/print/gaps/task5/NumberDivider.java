7e9c0afd80f758670b4121b6289e36dffde4c5c4
BigVito7 <biggvito7@gmail.com>
2025-02-25 13:00:10 +0200

    Divide by 5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,20 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        // Perform division by 5
+        double result = (double) toBeDivided / 5;
+
+        // Print the result
+        System.out.println("The result of dividing " + toBeDivided + " by 5 is: " + result);
     }
-}
+
+    public static void main(String[] args) {
+        // Create an instance of NumberDivider
+        NumberDivider divider = new NumberDivider();
+
+        // Test the divide method
+        divider.divide(20); // Example: Divide 20 by 5
+    }
+    }
+
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        // Convert minutes to seconds
+        float seconds = minutes * 60;
+
+        // Print the result
+        System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of TimeConvertor
+        TimeConvertor converter = new TimeConvertor();
+
+        // Test the convert method
+        converter.convert(5.0f); // Example: Convert 5 minutes to seconds
     }
 }
